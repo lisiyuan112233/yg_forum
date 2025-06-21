889d8e54c49c07572c7ef578ffc54b64b06ebc9e
lisiyuan <3215632117@qq.com>
2025-06-21 08:38:00 +0800

    aa

---
+++ src/test/java/com/sia/yg_forum/YgForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sia.yg_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
